7309e061fe9d011edbd6584667def8cb3b418a6a
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-05-12 22:12:16 +0300

    Allow origin

--- v1/config/allowedOrigins.js
+++ v1/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://192.168.1.8:5002",
   "http://localhost:5002",
   "https://localhost:5002",
+  "https://lamasa.onrender.com",
 ];
 
 module.exports = allowedOrigins;
